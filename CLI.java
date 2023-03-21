5cc9f5bc6f4f343ae5cdeda8821cafaa3920f37b
AXm0us <74784731+Ahmed-Mohammed-11@users.noreply.github.com>
2023-03-21 13:54:48 +0200

    Update CLI.java

--- CLI.java
+++ CLI.java
@@ -3,72 +3,74 @@ import java.util.Scanner;
 
 public class CLI {
     public static void main(String[] args) throws IOException {
-        System.out.println("please choose type of Dictionary:\nAVL Tree-->1\nRB Tree-->2");
-        Scanner sc= new Scanner(System.in);
+        System.out.println("please choose type of Dictionary:\nAVL Tree --> 1\nRB Tree --> 2");
+        System.out.print("choose tree you prefer: ");
+        Scanner sc = new Scanner(System.in);
         Boolean st = true;
-        String ch="",w ;
-        while(st){
+        String ch = "", w;
+        while (st) {
             ch = sc.nextLine().replaceAll("\\ ", "");
-            if (ch.equals("1")||ch.equals("2")){
+            if (ch.equals("1") || ch.equals("2")) {
                 st = false;
-            }
-            else{
+            } else {
                 System.out.println("please enter a valid choice :(\n");
             }
         }
         st = true;
         English_Dictionary D = new English_Dictionary(ch);
-        System.out.println("Choose what you want to do:\n" +
-                "Insert-->1\n" +
-                "Delete-->2\n" +
-                "Search-->3\n" +
-                "Batch Insert-->4\n" +
-                "Batch Delete-->5\n" +
-                "Size-->6\n" +
-                "Tree Height-->7\n" +
-                "Show Dictionary-->8" +
-                "Show choices-->9\n" +
-                "Make a new Dictionary-->10\n");
-        while(st){
+        System.out.println("Choose what you want to do: \n" +
+                "Insert --> 1\n" +
+                "Delete --> 2\n" +
+                "Search --> 3\n" +
+                "Batch Insert --> 4\n" +
+                "Batch Delete --> 5\n" +
+                "Size --> 6\n" +
+                "Tree Height --> 7\n" +
+                "Show Dictionary --> 8\n" +
+                "Show choices --> 9\n" +
+                "Make a new Dictionary --> 10\n");
+
+        while (st) {
+            System.out.print("choose an action: ");
             ch = sc.nextLine().replaceAll("\\ ", "");
-            switch (ch){
+            switch (ch) {
                 case "1":
-                    System.out.println("Enter the word you want to insert:");
+                    System.out.print("Enter the word you want to insert: ");
                     w = sc.nextLine().replaceAll("\\ ", "");
-                    if (w.contains("(")||w.contains(")")||w.contains("[")||w.contains("]")||w.contains("{")||w.contains("}")){
+                    if (w.contains("(") || w.contains(")") || w.contains("[") || w.contains("]") || w.contains("{")
+                            || w.contains("}")) {
                         System.out.println("please enter a valid word :(\n");
-                    }
-                    else {
+                    } else {
                         D.Insert(w);
                     }
                     break;
                 case "2":
-                    System.out.println("Enter the word you want to Delete");
+                    System.out.print("Enter the word you want to delete: ");
                     w = sc.nextLine().replaceAll("\\ ", "");
-                    if (w.contains("(")||w.contains(")")||w.contains("[")||w.contains("]")||w.contains("{")||w.contains("}")){
+                    if (w.contains("(") || w.contains(")") || w.contains("[") || w.contains("]") || w.contains("{")
+                            || w.contains("}")) {
                         System.out.println("please enter a valid word :(\n");
-                    }
-                    else {
+                    } else {
                         D.Delete(w);
                     }
                     break;
                 case "3":
-                    System.out.println("Enter the word you want to Search for");
+                    System.out.print("Enter the word you want to search for: ");
                     w = sc.nextLine().replaceAll("\\ ", "");
-                    if (w.contains("(")||w.contains(")")||w.contains("[")||w.contains("]")||w.contains("{")||w.contains("}")){
+                    if (w.contains("(") || w.contains(")") || w.contains("[") || w.contains("]") || w.contains("{")
+                            || w.contains("}")) {
                         System.out.println("please enter a valid word :(\n");
-                    }
-                    else {
+                    } else {
                         D.Search(w);
                     }
                     break;
                 case "4":
-                    System.out.println("Enter the txt path:");
+                    System.out.print("Enter file path: ");
                     w = sc.nextLine();
                     D.BInsert(w);
                     break;
                 case "5":
-                    System.out.println("Enter the txt path:");
+                    System.out.print("Enter file path: ");
                     w = sc.nextLine();
                     D.BDelete(w);
                     break;
@@ -83,27 +85,27 @@ public class CLI {
                     break;
                 case "9":
                     System.out.println("Choose what you want to do:\n" +
-                            "Insert-->1\n" +
-                            "Delete-->2\n" +
-                            "Search-->3\n" +
-                            "Batch Insert-->4\n" +
-                            "Batch Delete-->5\n" +
-                            "Size-->6\n" +
-                            "Tree Height-->7\n" +
-                            "Show Dictionary-->8" +
-                            "Show choices-->9\n" +
-                            "Make a new Dictionary-->10\n");
+                            "Insert --> 1\n" +
+                            "Delete --> 2\n" +
+                            "Search --> 3\n" +
+                            "Batch Insert --> 4\n" +
+                            "Batch Delete --> 5\n" +
+                            "Size --> 6\n" +
+                            "Tree Height --> 7\n" +
+                            "Show Dictionary --> 8\n" +
+                            "Show choices --> 9\n" +
+                            "Make a new Dictionary --> 10\n");
                     break;
                 case "10":
-                    System.out.println("please choose type of Dictionary:\nAVL Tree-->1\nRB Tree-->2");
+                    System.out.println("please choose type of Dictionary:\nAVL Tree --> 1\nRB Tree --> 2");
+                    System.out.print("choose the tree you prefer: ");
                     boolean f = true;
                     String ty = "";
-                    while(f){
+                    while (f) {
                         ty = sc.nextLine().replaceAll("\\ ", "");
-                        if (ty.equals("1")||ty.equals("2")){
+                        if (ty.equals("1") || ty.equals("2")) {
                             f = false;
-                        }
-                        else{
+                        } else {
                             System.out.println("please enter a valid choice :(\n");
                         }
                     }
